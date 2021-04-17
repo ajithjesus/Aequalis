ad88a03750e783fa5fcc24c9ef1b440f432d41ae
ajithjesus <ajithanandan2306@gmail.com>
2021-04-17 11:09:11 +0530

    task update

--- src/Task1/Task1.js
+++ src/Task1/Task1.js
@@ -60,117 +60,98 @@ const Task1 = () => {
         </Row>
         <Row>
           <Col md={{ span: 8, offset: 2 }} className="form_wrap">
-              <div className="form_table_container">
-
+            <div className="form_table_container">
               <Row>
-
-           
-          
-          
-                  <Col md={3} xs={12}>
+                <Col md={3} xs={12}>
                   <input
-                  type="text"
-                  name="id"
-                  placeholder="id"
-                  value={inputValues.id}
-                  onChange={textinputchange}
-                />
-                  </Col>
-                  <Col md={3} xs={12}>
+                    type="text"
+                    name="id"
+                    placeholder="id"
+                    value={inputValues.id}
+                    onChange={textinputchange}
+                  />
+                </Col>
+                <Col md={3} xs={12}>
                   <input
-                  type="text"
-                  name="username"
-                  placeholder="username"
-                  value={inputValues.username}
-                  onChange={textinputchange}
-                />
-                  </Col>
-                  <Col md={3} xs={12}>
-                         
-                <input
-                  type="text"
-                  name="lastname"
-                  placeholder="lastname"
-                  value={inputValues.lastname}
-                  onChange={textinputchange}
-                />
-                      </Col>
-                      <Col md={3} xs={12}>
-                      
-                <Button
-                  variant="primary"
-                  className="Add-button"
-                  onClick={AddData}
-                >
-                  Add
-                </Button>
-                      </Col>
-              
-               
-             
-            
-                      </Row>
-              </div>
-
-          
-           
+                    type="text"
+                    name="username"
+                    placeholder="username"
+                    value={inputValues.username}
+                    onChange={textinputchange}
+                  />
+                </Col>
+                <Col md={3} xs={12}>
+                  <input
+                    type="text"
+                    name="lastname"
+                    placeholder="lastname"
+                    value={inputValues.lastname}
+                    onChange={textinputchange}
+                  />
+                </Col>
+                <Col md={3} xs={12}>
+                  <Button
+                    variant="primary"
+                    className="Add-button"
+                    onClick={AddData}
+                  >
+                    Add
+                  </Button>
+                </Col>
+              </Row>
+            </div>
           </Col>
         </Row>
 
         <Row>
-        <Col md={{ span: 8, offset: 2 }}>
+          <Col md={{ span: 8, offset: 2 }}>
             <Row>
-            {
-                datas.length ===0 ?
+              {datas.length === 0 ? (
                 <div className="Record_notFound">
-                    <p>Data Records Not Found</p>
-                </div>:
-           
-            <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>First Name</th>
-            <th>Password</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-          {datas.map((newdata, index) => {
-            return (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{newdata.username}</td>
-                <td>{newdata.lastname}</td>
-                <td>
-                  <i
-                    className="fa fa-pencil-square"
-                    aria-hidden="true"
-                    onClick={() => handletoEdit(newdata)}
-                  ></i>
-                </td>
-                <td>
-                  <i
-                    className="fa fa-trash"
-                    aria-hidden="true"
-                    onClick={() => handletoRemove(newdata.id)}
-                  ></i>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-       }
-    
+                  <p>Data Records Not Found</p>
+                </div>
+              ) : (
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>id</th>
+                      <th>First Name</th>
+                      <th>Password</th>
+                      <th>Edit</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {datas.map((newdata, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td>{newdata.username}</td>
+                          <td>{newdata.lastname}</td>
+                          <td>
+                            <i
+                              className="fa fa-pencil-square"
+                              aria-hidden="true"
+                              onClick={() => handletoEdit(newdata)}
+                            ></i>
+                          </td>
+                          <td>
+                            <i
+                              className="fa fa-trash"
+                              aria-hidden="true"
+                              onClick={() => handletoRemove(newdata.id)}
+                            ></i>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              )}
             </Row>
-            </Col>
+          </Col>
         </Row>
       </Container>
-
-      
     </div>
   );
 };
